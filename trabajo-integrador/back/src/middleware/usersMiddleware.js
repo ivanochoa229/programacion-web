326c7e8aaa2af0f709763a6bb6d4cedf1228c4e8
Cabaleiro Nicolas <nicocabaleiro2002@gmail.com>
2024-11-19 14:06:05 -0300

    Se elimino console.log('hole')

--- trabajo-integrador/back/src/middleware/usersMiddleware.js
+++ trabajo-integrador/back/src/middleware/usersMiddleware.js
@@ -23,7 +23,7 @@ const validateToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
   const token = authHeader && authHeader.split(" ")[1];
     if (!token) {
-      console.log('hole');
+    
         return res.status(401).json({ message: 'Authentication required' });
     }
 
